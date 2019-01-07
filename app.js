d83c5caf1e4d997d552ccc1528b016d4c5713ec3
boldobod <obodozie.simeon@gmail.com>
2019-01-07 17:36:06 +0100

    app.js update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
-app.use(require('./controllers'));
+app.use('./server/controllers');
 
 // eslint-disable-next-line func-names
 app.listen('3000', () => {
