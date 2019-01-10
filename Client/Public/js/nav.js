0f0226524c4098ca863162df02e71e8b588c56df
boldobod <obodozie.simeon@gmail.com>
2019-01-10 22:34:55 +0100

    [Chore

--- Client/Public/js/nav.js
+++ Client/Public/js/nav.js
@@ -1,32 +1,3 @@
-// $(document).ready(){
-
-//     $(".dot-nav").on("click", function){
-        
-//     }
-// }
-
-// function toggleDiv(id) {
-//     var aside = document.getElementById(id);
-//     console.log('aside',aside.style)
-//     if (aside.style.visibility === 'hidden'){
-//         aside.style.visibility = 'hidden'
-//     }
-//     else {
-//         aside.style.visibility === 'hidden'
-//     }
-// };
-
-// document.getElementById("sidebar").addEventListener("click", toggleDiv(){
-//     var aside = document.getElementById(id);
-
-//     if (aside.style.visibility === 'hidden'){
-//         aside.style.visibility = 'hidden'
-//     }
-//     else {
-//         aside.style.visibility === 'hidden'
-//     }
-// });
-
 function toggleDiv(id) {
     let aside  = document.getElementById(id);
 if(aside.style.visibility === "hidden") {
